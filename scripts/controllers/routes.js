c085066d1edca45e2c63280cd7207637083ace7d
MMehelich <cassiel8198@gmail.com>
2018-03-04 17:47:49 -0800

    Not complete but I tried.

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,14 +1,20 @@
 'use strict';
 
 
-page('/', () => app.Book.fetchAll(app.bookView.initIndexPage));
+page('/', (ctx, next) => app.Book.fetchAll(() => app.bookView.initIndexPage(ctx, next))
+  , (ctx, next) => app.adminView.verify(ctx, next)
+);
 page('/books/add', ctx => app.bookView.initCreateFormPage(ctx));
 
-page('/books/:id', ctx => app.Book.fetchOne(ctx, app.bookView.initDetailPage));
+page('/books/:id', (ctx, next) => app.Book.fetchOne(ctx, () => app.bookView.initDetailPage(ctx, next))
+  , (ctx, next) => app.adminView.verify(ctx, next)
+);
 
-page('/admin',
-  (ctx, next) => app.adminView.initAdminPage(ctx, next),
-  (ctx) => app.adminView.verify(ctx));
-// page('/books/:book_id', ctx => whateverCallbackYouWant(ctx, anotherCallback));
+page('/books/:id/update'
+  , (ctx, next) => app.Book.fetchOne(ctx, next)
+  , ctx => app.bookView.initUpdateFormPage(ctx)
+);
+
+page('/admin', () => app.adminView.initAdminPage());
 
 page();
